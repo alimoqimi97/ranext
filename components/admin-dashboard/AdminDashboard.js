0f2d0ef2ee6c91990ca6b533c36e223a0bac9499
alimoqimi97 <alimoqimi97@gmail.com>
2021-11-04 16:23:20 +0330

    posts and lists resources created

--- components/admin-dashboard/AdminDashboard.js
+++ components/admin-dashboard/AdminDashboard.js
@@ -1,15 +1,16 @@
 import * as React from "react";
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
-import UsersList from '../users/UsersList.js';
+import { UsersList } from '../users/users.js';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 const AdminDashboard = () => {
     return (
     <Admin dataProvider={dataProvider} >
+        {/* <Resource name="posts" list={UsersList} /> */}
+        <Resource name="posts" list={ListGuesser} />
         <Resource name="users" list={ListGuesser} />
-        <Resource name="posts" list={UsersList} />
     </Admin>
     );
 };
--- components/users/UsersList.js
+++ components/users/UsersList.js
@@ -1,18 +1,36 @@
+// import * as React from "react";
+// import { List, Datagrid, TextField, EmailField } from 'react-admin';
+// import MyUrlField from '../MyUrlField/MyUrlField.js';
+
+// export const UserList = props => {
+//     return (
+//         <List {...props}>
+//             <Datagrid rowClick="edit">
+//                 <TextField source="id" />
+//                 <TextField source="name" />
+//                 <EmailField source="email" />
+//                 <TextField source="phone" />
+//                 <MyUrlField source="website" />
+//                 <TextField source="company.name" />
+//             </Datagrid>
+//         </List>
+//     );
+// };
+
+
 import * as React from "react";
 import { List, Datagrid, TextField, EmailField } from 'react-admin';
 import MyUrlField from '../MyUrlField/MyUrlField.js';
 
-export const UserList = props => {
-    return (
-        <List {...props}>
-            <Datagrid rowClick="edit">
-                <TextField source="id" />
-                <TextField source="name" />
-                <EmailField source="email" />
-                <TextField source="phone" />
-                <MyUrlField source="website" />
-                <TextField source="company.name" />
-            </Datagrid>
-        </List>
-    );
-};
+export const UserList = props => (
+    <List {...props}>
+        <Datagrid rowClick="edit">
+            <TextField source="id" />
+            <TextField source="name" />
+            <EmailField source="email" />
+            <TextField source="phone" />
+            <MyUrlField source="website" />
+            <TextField source="company.name" />
+        </Datagrid>
+    </List>
+);
