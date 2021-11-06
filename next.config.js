634d5ee6999d0c4806b6848b5b291295595ff854
alimoqimi97 <alimoqimi97@gmail.com>
2021-11-06 15:10:14 +0330

    bugs fixed and next pwa created successfully...

--- next.config.js
+++ next.config.js
@@ -1,14 +1,25 @@
 const withPWA = require("next-pwa");
 
-
-module.exports = {
-  reactStrictMode: true,
-}
-
 module.exports = withPWA({
+  reactStrictMode: true,
   pwa: {
     dest: "public",
     register: true,
     skipWaiting: true,
-  },
+    // disable: process.env.NODE_ENV === 'developement'
+  }
+
 });
+
+
+// module.exports = {
+//   reactStrictMode: true,
+// }
+
+// module.exports = withPWA({
+//   pwa: {
+//     dest: "public",
+//     register: true,
+//     skipWaiting: true,
+//   },
+// });
