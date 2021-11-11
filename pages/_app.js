08737d67fb0868f23f88cf99b81f61b4155189e3
alimoqimi97 <alimoqimi97@gmail.com>
2021-11-11 12:15:03 +0330

    rtl added to test project

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return (<Component {...pageProps} >
 
-          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+          {/* <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/> */}
           </Component>);
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -3,12 +3,12 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html dir={'rtl'}  >
         <Head>
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
           <meta name="theme-color" content="#fff" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+          {/* <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/> */}
         </Head>
         <body>
           <Main />
